241b3093feb83a4e17c86521652628d04ef4ee34
Andrew Novac <leet-shadow@hotmail.com>
2018-12-12 15:54:27 -0500

    Create rgba_to_rgb.cpp

---
+++ Raster Images/src/rgba_to_rgb.cpp
@@ -0,0 +1,11 @@
+#include "rgba_to_rgb.h"
+
+void rgba_to_rgb(
+  const std::vector<unsigned char> & rgba,
+  const int & width,
+  const int & height,
+  std::vector<unsigned char> & rgb)
+{
+  rgb.resize(height*width*3);
+  //
+}
