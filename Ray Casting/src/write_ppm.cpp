14c3a6f50d0f77c3e2a784904df342bcd5c5ac1c
Tenumbra <leet-shadow@hotmail.com>
2019-02-07 09:20:14 -0500

    Initalized Ray Casting

---
+++ Ray Casting/src/write_ppm.cpp
@@ -0,0 +1,18 @@
+#include "write_ppm.h"
+#include <fstream>
+#include <cassert>
+#include <iostream>
+
+bool write_ppm(
+  const std::string & filename,
+  const std::vector<unsigned char> & data,
+  const int width,
+  const int height,
+  const int num_channels)
+{
+  ////////////////////////////////////////////////////////////////////////////
+  // Replace with your code from computer-graphics-raster-images or email
+  // jacobson@cs.toronto.edu for correct code.
+  return false;
+  ////////////////////////////////////////////////////////////////////////////
+}
