8b78f0177ece45cc9d69ed28a9471ebf982d45ec
Tenumbra <leet-shadow@hotmail.com>
2019-02-14 10:12:52 -0500

    Plane intersection clause

--- Ray Casting/src/Plane.cpp
+++ Ray Casting/src/Plane.cpp
@@ -4,9 +4,22 @@
 bool Plane::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
+  Eigen::Vector3d d = ray.direction;
+  Eigen::Vector3d e = ray.origin;
+  
+  // if parallel, no intersection
+  if(d.dot(normal) == 0){
+    return false;
+  }
+  
+  // have intersection, calculate t
+  double temp_t = (point - e).dot(normal) / d.dot(normal);
+  if(temp_t >= min_t){
+    t = temp_t;
+    n = normal;
+    return true;
+  }
+  
   return false;
-  ////////////////////////////////////////////////////////////////////////////
+  
 }
-
