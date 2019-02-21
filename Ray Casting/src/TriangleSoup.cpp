26a6ec83fda94cc4d25284fafc5be48f49c16b57
Tenumbra <leet-shadow@hotmail.com>
2019-02-21 10:10:59 -0500

    Create soup from several triangles

--- Ray Casting/src/TriangleSoup.cpp
+++ Ray Casting/src/TriangleSoup.cpp
@@ -6,10 +6,11 @@
 bool TriangleSoup::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  // formed with several triangle objects
+  int hit_id = 0;
+  bool result = first_hit(ray, min_t, triangles, hit_id, t, n);
+  
+  return result;
 }
 
 
