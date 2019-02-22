ffa992ef2c1e18c42e0765bc2c30d3078ccf401b
Tenumbra <leet-shadow@hotmail.com>
2019-02-22 09:46:49 -0500

    Vision view added

--- Ray Casting/src/viewing_ray.cpp
+++ Ray Casting/src/viewing_ray.cpp
@@ -8,8 +8,20 @@ void viewing_ray(
   const int height,
   Ray & ray)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  // origin
+  ray.origin = camera.e;
+  
+  // edges
+  double r = 0.5 * camera.width;
+  double l = -0.5 * camera.width;
+  double t = -0.5 * camera.height;
+  double b = 0.5 * camera.height;
+  
+  // position u v
+  double u = l + (r - l) * (j + 0.5) / width;
+  double v = b + (t - b) * (i + 0.5) / height;
+  
+  // direction
+  ray.direction = - camera.d * camera.w + camera.u * u + camera.v * v;
 }
 
