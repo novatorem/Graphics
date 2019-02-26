e531b89573f2f0065dd1df9447e493d70730d209
Tenumbra <leet-shadow@hotmail.com>
2019-02-26 09:29:10 -0500

    Added hitting

--- Ray Casting/src/first_hit.cpp
+++ Ray Casting/src/first_hit.cpp
@@ -8,9 +8,27 @@ bool first_hit(
   double & t,
   Eigen::Vector3d & n)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
-}
+  // initialize
+  bool hit = false;
+  t = std::numeric_limits<double>::max();
+  double curr_t;
+  Eigen::Vector3d curr_n;
 
+  // loop through each object
+  for (int i=0; i<objects.size(); i++){
+    // if such object intersect
+    if(objects[i]->intersect(ray, min_t, curr_t, curr_n)){
+      
+      // update if it's an earlier hit
+      if(curr_t < t){
+        t = curr_t;
+        hit = true;
+        n = curr_n;
+        hit_id = i;
+      }
+    }
+  }
+
+  return hit;
+
+}
