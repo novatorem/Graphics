f54b0d42f8152fa37e75c19be60bf96895a50a42
Tenumbra <leet-shadow@hotmail.com>
2019-03-01 16:07:11 -0500

    Added ppm

--- Ray Casting/src/write_ppm.cpp
+++ Ray Casting/src/write_ppm.cpp
@@ -10,9 +10,34 @@ bool write_ppm(
   const int height,
   const int num_channels)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code from computer-graphics-raster-images or email
-  // jacobson@cs.toronto.edu for correct code.
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+// initialize the file needed
+  std::ofstream file;
+  file.open(filename, std::ios::out);
+  if(!file){
+    std::cout << "Can't open ppm file\n";
+    return false;
+  }
+  
+  // PPM header
+  file << "P3 " << '\n' << width << '\n' << height << '\n' << "255\n";
+  
+  // loop through each pixel
+  for (int idx=0; idx<width*height; idx++){
+    // rgb image
+    if (num_channels == 3){
+      file << (int)data[idx*3] << ' ';
+      file << (int)data[idx*3 + 1] << ' ';
+      file << (int)data[idx*3 + 2] << ' ';
+    }
+    // grayscale image
+    else{
+      file << (int)data[idx] << ' ';
+      file << (int)data[idx] << ' ';
+      file << (int)data[idx] << ' ';
+    }
+  }
+  
+  // close the file
+  file.close();
+  return true;
 }
