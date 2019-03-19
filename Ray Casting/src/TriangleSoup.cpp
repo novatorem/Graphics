9ccc6f99b0d040523794964ffd097bd75ffea158
Tenumbra <leet-shadow@hotmail.com>
2019-03-19 10:08:12 -0400

    Multi triangular formulation

--- Ray Casting/src/TriangleSoup.cpp
+++ Ray Casting/src/TriangleSoup.cpp
@@ -1,12 +1,11 @@
 #include "TriangleSoup.h"
 #include "Ray.h"
-// Hint
 #include "first_hit.h"
 
 bool TriangleSoup::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
-  // formed with several triangle objects
+  // Multi-triangular forumuation
   int hit_id = 0;
   bool result = first_hit(ray, min_t, triangles, hit_id, t, n);
   
