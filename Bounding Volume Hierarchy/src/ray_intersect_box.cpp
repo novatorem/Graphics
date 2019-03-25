7228e41478642b7e14bf3a7640b80263fa04fdbb
Tenumbra <leet-shadow@hotmail.com>
2019-03-25 10:35:24 -0400

    Added Bounding Volume Hierarcy

---
+++ Bounding Volume Hierarchy/src/ray_intersect_box.cpp
@@ -0,0 +1,14 @@
+#include "ray_intersect_box.h"
+#include <iostream>
+
+bool ray_intersect_box(
+  const Ray & ray,
+  const BoundingBox& box,
+  const double min_t,
+  const double max_t)
+{
+  ////////////////////////////////////////////////////////////////////////////
+  // Replace with your code here:
+  return false;
+  ////////////////////////////////////////////////////////////////////////////
+}
